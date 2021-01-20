b29aaef1616cca4bb5ce61b371daccbfed860dd0
fmmenezes <fmmenezes@hotmail.com>
2021-01-20 16:54:48 -0300

    Alteracao no arquivo principal da aplicacao

---
+++ src/test/java/com/mm/meuprimeiroprojeto/MeuPrimeiroProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mm.meuprimeiroprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuPrimeiroProjetoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
